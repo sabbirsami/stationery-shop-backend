09117e2af01743f00cf51b040bf63aa11b8a3edd
SabbirSami <smd71430@gmail.com>
2024-11-22 08:05:25 +0600

    feat: add product model

---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,46 @@
+import { model, Schema } from 'mongoose';
+import { ProductType } from './product.interface';
+
+const productSchema = new Schema<ProductType>({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  brand: {
+    type: String,
+    required: [true, 'Brand name is required'],
+  },
+
+  price: {
+    type: Number,
+    required: [true, 'Price is required'],
+    min: [0, 'Price cannot be less then zero'],
+  },
+
+  category: {
+    type: String,
+    enum: {
+      values: [
+        'Writing',
+        'Office Supplies',
+        'Art Supplies',
+        'Educational',
+        'Technology',
+      ],
+      message:
+        'Category must be one of Writing, Office Supplies, Art Supplies, Educational, or Technology.',
+    },
+    required: [true, 'Category is required'],
+  },
+  quantity: {
+    type: Number,
+    required: [true, 'Quantity is required'],
+    min: [0, 'Quantity cannot be less then zero'],
+  },
+  inStock: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+export const ProductModal = model('Product', productSchema);
